668fef46b5d7f1aa31724ec82f3dfeed480b3ccf
Sai Pravallika Kuppa (Accenture International Limite) <v-skuppa@microsoft.com>
2025-03-17 06:46:45 +0000

    Merged PR 699789: Enabled Code Coverage and Added azurepipelines-coverage.yml for QCS requirement
    - Updatedrun-tests.cmd and OneBranch.PR.yml to enable code coverage collection
    - Added azurepipelines-coverage.yml for QCS requirement
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Code coverage enhancement
    
    #### PR Summary
    This pull request enables code coverage for the test execution in the repository.
    - `scripts/run-tests.cmd`: Added the `/collect:"Code Coverage;Format=Cobertura"` flag to the test execution command to enable code coverage.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    - Added `azurepipelines-coverage.yml` to configure code coverage status and comments for pull requests with a target of 50% diff coverage.
    
    Related work items: #1635307

--- scripts/run-tests.cmd
+++ scripts/run-tests.cmd
@@ -5,7 +5,7 @@ call .\init-dev-cmd.cmd
 echo ========================================
 echo "Run PowerBI.Api.Tests ..."
 echo ========================================
-call "vstest.console" /logger:trx /Platform:x64 /InIsolation %~dp0..\sdk\PowerBI.Api.Tests\bin\Release\PowerBI.Api.Tests.dll /framework:framework40 /ResultsDirectory:%~dp0..\TestResults
+call "vstest.console" /logger:trx /Platform:x64 /collect:"Code Coverage;Format=Cobertura" /InIsolation %~dp0..\sdk\PowerBI.Api.Tests\bin\Release\PowerBI.Api.Tests.dll /framework:framework40 /ResultsDirectory:%~dp0..\TestResults
 
 set EX=%ERRORLEVEL%
 
